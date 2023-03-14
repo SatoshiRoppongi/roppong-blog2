8b850029434245a009a51e8fb1ba24d9bf3c1dff
Satoshi Roppongi <satoshiroppongi@gmail.com>
2023-03-14 18:48:38 +0900

    improve 見た目

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -46,8 +46,49 @@ onMounted(() => {
     }
 
     ::v-deep pre {
+        margin: 1em 0;
+        padding: 1em;
+        border-radius: 5px;
+        background: #25292f;
+        color: #fff;
         overflow-x:scroll;
+        -webkit-overflow-scrolling: touch; 
 
+    }
+    ::v-deep p {
+        line-height: 3em;
+    }
+    ::v-deep li {
+        line-height: 2em;
+    }
+
+    ::v-deep h2 {
+        margin: 3em 0;
+        padding: 0.5em;/*文字周りの余白*/
+        color: #494949;/*文字色*/
+        background: #fffaf4;/*背景色*/
+        border-left: solid 5px #ffaf58;/*左線（実線 太さ 色）*/
+    }
+
+    ::v-deep h3 {
+        /*線の種類（実線） 太さ 色*/
+        border-bottom: solid 3px black;
+    }
+
+    ::v-deep .table-of-contents {
+        background: #f3f3f3;
+        border: 1px solid #ccc;
+        padding: 5px 10px;
+        margin: 0 0 20px;
+        width: fit-content;
+        ul {
+        padding-left: 24px;
+        }
+        &:before {
+        content: '目次';
+        font-weight: bold;
+        font-size: 16px;
+        }
     }
 }
 
