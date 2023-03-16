57fddf4cac0a10a33b25c3118b278aa0d0a106b6
Satoshi Roppongi <satoshiroppongi@gmail.com>
2023-03-16 18:45:51 +0900

    markdown周りの表示設定

--- @types/markdownittoc.d.ts
+++ @types/markdownittoc.d.ts
@@ -1,2 +1,7 @@
 declare module "markdown-it-table-of-contents";
+declare module "markdown-it-video"
+declare module "markdown-it-podcast"
+declare module "markdown-it-footnote"
+declare module "markdown-it-sanitizer"
+declare module "markdown-it-toc-done-right"
 // 余裕があれば型定義ファイルを作成する
--- plugins/markdownit.ts
+++ plugins/markdownit.ts
@@ -2,11 +2,40 @@
 import md from "markdown-it";
 import mdAnchor from 'markdown-it-anchor';
 import mdToc from 'markdown-it-table-of-contents';
+import mdVideo from 'markdown-it-video';
+import mdPodcast from 'markdown-it-podcast';
+import mdSanitizer from 'markdown-it-sanitizer';
+import mdFootnote from 'markdown-it-footnote';
+import mdTocDoneRight from 'markdown-it-toc-done-right';
 
 export default defineNuxtPlugin(() => {
-  const renderer = md();
-  renderer.use(mdAnchor)
+  const renderer = md({
+    breaks: true,
+    html: true,
+    linkify: true,
+    typographer: true,
+    xhtmlOut: true,
+    langPrefix: 'language-'
+  });
   renderer.use(mdToc)
+  renderer.use(mdVideo)
+  renderer.use(mdSanitizer)
+  renderer.use(mdFootnote)
+  renderer.use(mdPodcast, {
+    url: '',
+    height: 200,
+    auto_play: false,
+    hide_related: false,
+    show_comments: false,
+    show_user: true,
+    show_reports: false,
+    visual: true,
+  })
+  renderer.use(mdAnchor, {
+    permalink: true,
+    permalinkBefore: true,
+    permalinkSymbol: '§',
+  }).use(mdTocDoneRight)
 
   return {
     provide: {
