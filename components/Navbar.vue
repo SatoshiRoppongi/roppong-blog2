32db4678a99efad058c9483390bc2d3d75d0f3d9
Satoshi Roppongi <satoshiroppongi@gmail.com>
2023-03-22 12:43:01 +0900

    wrap el-menu by client-only

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -19,45 +19,47 @@ const handleSelect = (key: string, keyPath: string[]) => {}
 
 </script>
 <template>
-    <el-menu
-        :default-active="activeIndex"
-        class="el-menu"
-        mode="horizontal"
-        @select="handleSelect"
-    >
-    <!-- fixme: メニューの端っこをクリックしても遷移しない -->
-        <el-menu-item index="blog">
-            <NuxtLink to="/blog" tag="div" class="c-p">
-            <el-icon><HomeFilled /></el-icon>
-            ホーム
-            </NuxtLink>
-        </el-menu-item>
-        <el-menu-item v-for="cat in getContentsSummaries('category')" :key="cat.slug" :index="cat.slug">
-            <NuxtLink :to="`/blog/category/${cat.slug}`" class="c-p">
-                <el-icon v-if="cat.slug">
-                    <component v-bind:is="$iconComponent(cat.slug)" />
-                </el-icon>
-                {{ cat.title }}
-            </NuxtLink>
-        </el-menu-item>
-        <div class="flex-grow" />
-        <el-menu-item index="about">
-            <NuxtLink to="/blog/about" tag="div" class="c-p">
-                <el-icon>
-                    <InfoFilled />
-                </el-icon>
-                このブログについて
-            </NuxtLink>
-        </el-menu-item>
-        <el-menu-item index="contact">
-            <NuxtLink to="/blog/contact" tag="div" class="c-p">
-                <el-icon>
-                    <Promotion />
-                </el-icon>
-                お問い合わせ
-            </NuxtLink>
-        </el-menu-item>
-    </el-menu>
+    <client-only>
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu"
+            mode="horizontal"
+            @select="handleSelect"
+        >
+        <!-- fixme: メニューの端っこをクリックしても遷移しない -->
+            <el-menu-item index="blog">
+                <NuxtLink to="/blog" tag="div" class="c-p">
+                <el-icon><HomeFilled /></el-icon>
+                ホーム
+                </NuxtLink>
+            </el-menu-item>
+            <el-menu-item v-for="cat in getContentsSummaries('category')" :key="cat.slug" :index="cat.slug">
+                <NuxtLink :to="`/blog/category/${cat.slug}`" class="c-p">
+                    <el-icon v-if="cat.slug">
+                        <component v-bind:is="$iconComponent(cat.slug)" />
+                    </el-icon>
+                    {{ cat.title }}
+                </NuxtLink>
+            </el-menu-item>
+            <div class="flex-grow" />
+            <el-menu-item index="about">
+                <NuxtLink to="/blog/about" tag="div" class="c-p">
+                    <el-icon>
+                        <InfoFilled />
+                    </el-icon>
+                    このブログについて
+                </NuxtLink>
+            </el-menu-item>
+            <el-menu-item index="contact">
+                <NuxtLink to="/blog/contact" tag="div" class="c-p">
+                    <el-icon>
+                        <Promotion />
+                    </el-icon>
+                    お問い合わせ
+                </NuxtLink>
+            </el-menu-item>
+        </el-menu>
+    </client-only>
 </template>
 <style scoped>
 .flex-grow {
