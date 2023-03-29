87879b5f68da6a36dc4eec0aa4c71ec651f482f6
Satoshi Roppongi <satoshiroppongi@gmail.com>
2023-03-29 17:51:16 +0900

    analytics.ts で指定するIDを修正

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -2,7 +2,7 @@ import VueGtag from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'UA-143514276-1'
+      id: 'G-RJ4HG4XLY5'
     }
   })
 })
